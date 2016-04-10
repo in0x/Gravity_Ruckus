1a46409dcfeba73a852931c2a49bdd9e41e93f71
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-04-10 15:28:39 +0200

    Added Script to controll Jump CD

---
+++ Assets/Scripts/Player/JumpController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpController : MonoBehaviour
+{
+    bool m_isJumping = false;
+   
+    public bool isJumping
+    {
+        get
+        {   if (!m_isJumping)
+            {
+                m_isJumping = true;
+                return false;
+            }
+
+            return true;
+        }
+
+        private set { m_isJumping = value; }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "LevelGeometry") m_isJumping = false;
+    }
+}
--- Assets/Scripts/Player/MovementHandler.cs
+++ Assets/Scripts/Player/MovementHandler.cs
@@ -3,21 +3,23 @@ using System.Collections;
 
 public class MovementHandler : MonoBehaviour
 {
-    public float m_fJumpHeight = 10f;
-    public float m_xLookMul = 1f;
-    public float m_yLookMul = 1f;
+    public float fJumpHeight = 10f;
+    public float xLookMul = 1f;
+    public float yLookMul = 1f;
+    public float moveSpeed = 150;
 
-    Vector3 posInput;
-    float xRotInput;
-    float yRotInput;
+    Vector3 m_posInput;
+    float m_xRotInput;
+    float m_yRotInput;
 
-    float speed = 150;
     Rigidbody m_RigidBody;
+    JumpController jumpController;
     Transform cam;
 
     void Start()
     {
         m_RigidBody = GetComponent<Rigidbody>();
+        jumpController = GetComponent<JumpController>();
 
         // Find the transform of the parents camera component
         foreach (Transform child in gameObject.transform)
@@ -25,16 +27,19 @@ public class MovementHandler : MonoBehaviour
             if (child.tag == "MainCamera") cam = child;
         }
     }
-
-    public void MoveInput(Vector3 move, Vector3 rote, bool jump = false)
+    
+    public void MoveInput(Vector3 move, Vector3 rote, bool wantsToJump = false)
     {
-        posInput = move;
-        xRotInput = rote.x;
-        yRotInput = rote.y;
+        m_posInput = move;
+        m_xRotInput = rote.x;
+        m_yRotInput = rote.y;
 
-        if (jump)
+        if (wantsToJump)
         {
-            posInput.y += m_fJumpHeight;
+            if (!jumpController.isJumping)
+            {
+                m_posInput.y += fJumpHeight;
+            }
         }
 
         ActualMove();
@@ -42,10 +47,10 @@ public class MovementHandler : MonoBehaviour
 
     void ActualMove()
     {
-        m_RigidBody.AddRelativeForce(posInput * speed);
+        m_RigidBody.AddRelativeForce(m_posInput * moveSpeed);
 
-        transform.localRotation *= Quaternion.Euler(0, xRotInput * m_xLookMul, 0);
-        Quaternion quat = Quaternion.Euler(yRotInput * m_yLookMul, 0, 0);
+        transform.localRotation *= Quaternion.Euler(0, m_xRotInput * xLookMul, 0);
+        Quaternion quat = Quaternion.Euler(m_yRotInput * yLookMul, 0, 0);
 
         if (cam.localRotation.eulerAngles.x > 80 && cam.localRotation.eulerAngles.x < 180 && quat.x < 0) // > 60 && < 180 && < 0
         {
