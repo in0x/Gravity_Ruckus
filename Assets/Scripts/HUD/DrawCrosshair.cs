bed0068f743783ce837b400c1656e359cfc40a15
Lukas Rosenberger <lrosenberger.mmt-b2014@fh-salzburg.ac.at>
2016-05-03 14:26:53 +0200

    merged stuff to wimmer_scene

--- Assets/Scripts/HUD/DrawCrosshair.cs
+++ Assets/Scripts/HUD/DrawCrosshair.cs
@@ -5,12 +5,20 @@ public class DrawCrosshair : MonoBehaviour
 {
     public Texture2D texture;
 
-    void OnGUI()
+    private float xMin;
+    private float yMin;
+
+    void Start()
     {
         Camera parentCamera = GetComponentInParent<Camera>();
         Rect cameraRect = parentCamera.pixelRect;
-        float xMin = cameraRect.xMin + (cameraRect.width / 2) - (texture.width / 8);
-        float yMin = cameraRect.yMin + (cameraRect.height / 2) - (texture.height / 8);
-        GUI.DrawTexture(new Rect(xMin, yMin, texture.width / 4, texture.height / 4), texture);
+        xMin = cameraRect.xMin + (cameraRect.width / 2) - (texture.width / 8f);
+        yMin = cameraRect.yMin + (cameraRect.height / 2) - (texture.height / 8f);
+    }
+
+    void OnGUI()
+    {
+        
+        GUI.DrawTexture(new Rect(xMin, yMin, texture.width / 4f, texture.height / 4f), texture);
     }
 }
