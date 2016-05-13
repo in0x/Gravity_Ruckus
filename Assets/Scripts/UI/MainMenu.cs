1869dcdcbfdac474b5791902a5fc8ee2124aad67
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2016-05-13 20:15:11 +0200

    Created scene with menu in final_level

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,6 +6,6 @@ public class MainMenu : MonoBehaviour
     public void QuickStartGame()
     {
         MatchProperties.customGame = false;
-        SceneManager.LoadScene("wimmer_scene");
+        SceneManager.LoadScene("final_level");
     }
 }
